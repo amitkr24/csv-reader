294f8c5d7fd518c8053c23ab20e907b4008d8e3d
amitkr24 <amitkr695@gmail.com>
2022-08-14 07:48:30 +0530

    csv listing

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,19 +1,19 @@
 const csv             = require('csv-parser')
 const fs              = require('fs')
-
+const LocalStorage    = require('node-localstorage').LocalStorage,
+localStorage          = new LocalStorage('./scratch');
 
 module.exports.index = function(req,res){
     const results         = [];
+    //local storgae
+    let fileData = localStorage.getItem('CsvFiles');
+    var nameArr = fileData.split(',');
     fs.createReadStream('./upload/sample.csv')
     .pipe(csv())
     .on('data', (data) => results.push(data))
     .on('end', () => {
       //console.log(results);
-      res.render('../view/index',{result:results})
-      // [
-      //   { NAME: 'Daffy Duck', AGE: '24' },
-      //   { NAME: 'Bugs Bunny', AGE: '22' }
-      // ]
+        res.render('../view/index',{result:results,fileData:nameArr})
     });
 }
 module.exports.upload = function ( req, res ) {
--- router/index.js
+++ router/index.js
@@ -1,21 +1,32 @@
 const express         = require('express');
 const router          = express.Router();
 const multer          = require('multer');
-
+const path            = require('path');
+const LocalStorage    = require('node-localstorage').LocalStorage,
+localStorage          = new LocalStorage('./scratch');
 //controller
 const homeController  = require('../controller/home_controller');
 console.log('router loaded');
+localStorage.setItem('CsvFiles', '');
+const myfiles = [];
 
 let storage = multer.diskStorage({
     destination: './uploads/',
     filename: function ( req, file, cb ) {
-        //req.body is empty...
-        //How could I get the new_file_name property sent from client here?
-        let fileName = file.originalname+ '-' + Date.now()+".csv";
+        // remove extension from file name
+        let file_name = path.parse(file.originalname).name;
+
+        //create new name of file
+        let fileName = file_name+ '-' + Date.now()+".csv";
         cb( null, fileName);
+        myfiles.push(fileName);
+        localStorage.setItem('CsvFiles', myfiles);
     }
 });
 let upload = multer( { storage: storage });
 router.route( '/upload_files' ).post( upload.single( 'file' ), homeController.upload);
-router.all('/', homeController.index);
+router.all('/:id?', homeController.index);
+
+
+
 module.exports = router;
